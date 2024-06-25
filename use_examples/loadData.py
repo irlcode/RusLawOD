d9f6d5982ab149a85e7b16ec2eea640b6cf794d2
denissaveliev <34233882+denissaveliev@users.noreply.github.com>
2024-06-25 19:36:58 +0300

    Update loadData.py

--- use_examples/loadData.py
+++ use_examples/loadData.py
@@ -1,65 +1,65 @@
 # -*- coding: utf-8 -*- 
 # By Denis A.Saveliev
-# This script loads RusLawOD data for a given year to a Pandas DataFrame
+# This script loads RusLawOD data to a Pandas DataFrame
 # It works in Python 3
 
 import pandas as pd
 import xml.etree.ElementTree as ET
 import glob
+import pyarrow
 
-year = '2011' # valid years 1991- 
-datasetdir = 'xml-texts/' #your path here
+work_dir = ''  # path
+datasetdir = work_dir + 'corpus_xml_full/'  # your path here
 
-#different types of work for different types of data 
-fieldsid = ['authorByOP',  'docDateByOP',  'docNumberByOP',   'pravogovruNd',  'IssuedByIPS',  'DocDateIPS' ,  'DocNumberIPS']
-fieldsop = ['opdate',  'opnumber', 'opweekcode']
-fieldshd = ['headingIPS', 'docTitleByOP', 'docTypeByOP']
-fieldstxt = ['textOCR',  'textIPS']
-fieldsclass = ['classifierByIPS',  'keywordByIPS']
-alldata = [] #we will collect dictionaries to this in order to make a Data Frame of it
+# different types of work for different types of data 
+fieldsid = ['pravogovruNd',
+            'issuedByIPS',
+            'docdateIPS' ,
+            'docNumberIPS',
+            'doc_typeIPS',
+            'headingIPS',
+            'doc_author_normal_formIPS',
+            'signedIPS',
+            'statusIPS',
+            'actual_datetimeIPS',
+            'actual_datetime_humanIPS',
+            'is_widely_used',
+            ]
+fieldshd = ['headingIPS', ]
+fieldstxt = ['taggedtextIPS',  'textIPS']
+fieldsclass = ['classifierByIPS',  'keywordsByIPS']
 
-#iterate through XML files, parse a file into a dictionary and add to alldata
-for inputfilename in glob.glob(datasetdir + str(year) + '*.xml'):
+def parse_file(inputfilename):
     tree = ET.parse(inputfilename)
     root = tree.getroot()
     bufferdict = {}
-    bufferdict['fileid'] = inputfilename[-15:]
     for field in fieldsid:
         try:
-            bufferdict[field] = root.find("./meta/identification/" + field).get('val')
+            bufferdict[field] = root.find(
+                "./meta/identification/" + field).get('val')
         except:
             bufferdict[field] = None
 
-    for field in fieldsop:
-        try:
-            bufferdict[field] = root.find("./meta/publication/").get(field)
-        except:
-            bufferdict[field] = None
-            
-    for field in fieldshd:
-        try:
-            bufferdict[field] = root.find("./meta/identification/" + field).text
-        except:
-            bufferdict[field] = None
 
     for field in fieldstxt:
         try:
             bufferdict[field] = root.find("./body/" + field).text
         except:
             bufferdict[field] = None
-    #then since we can have multiple values from classifier and keywords field we have to represent lists of them via semicolon
+    # then since we can have multiple values from classifier and keywords
+    # field we have to represent lists of them via semicolon
     kwall = ''
     try:
-        kw = root.findall("./meta/keywords/keywordByIPS")    
+        kw = root.findall("./meta/keywords/keywordsByIPS")    
         for i in kw:
             kwall = kwall + ';' + i.get('val')
         if kwall[0] == ';':
-            bufferdict['keywordByIPS'] = kwall[1:]        
+            bufferdict['keywordsByIPS'] = kwall[1:]        
     except:
-        bufferdict['keywordByIPS'] = None
+        bufferdict['keywordsByIPS'] = None
     clasall = ''
     try:
-        cla = root.findall("./meta/references/classifierByIPS")    
+        cla = root.findall("./meta/reference/classifierByIPS")    
         for i in cla:
             clasall = clasall + ';' + i.get('val')
         if clasall[0] == ';':
@@ -67,10 +67,27 @@ for inputfilename in glob.glob(datasetdir + str(year) + '*.xml'):
     except:
         bufferdict['classifierByIPS'] = None
             
-    alldata.append(bufferdict)
 
-# finally make a dataframe
-df = pd.DataFrame(alldata,  columns = ['fileid']+ fieldsid + fieldsop + fieldshd + fieldstxt + fieldsclass)
-# print some results:
-print(df.info())
-print(df['classifierByIPS'])
+    return bufferdict
+
+if __name__ == "__main__":
+    alldata = []  # we will collect dictionaries to this in order to
+    # make a Data Frame of it
+    counter = 0
+    # iterate through XML files, parse a file into a dictionary and add to alldata
+    for inputfilename in glob.glob(datasetdir  + '*.xml'):
+        bufferdict = parse_file(inputfilename)
+        alldata.append(bufferdict)
+        counter += 1
+        if counter % 30000 == 0:
+            print(counter)
+            df = pd.DataFrame(alldata,
+                              columns = fieldsid + fieldstxt + fieldsclass)
+            df.to_parquet(work_dir + 'ruslawod' + str(counter) + '.parquet',
+                          engine='pyarrow')
+            alldata = []
+
+    print(counter)
+    df = pd.DataFrame(alldata,  columns = fieldsid + fieldstxt + fieldsclass)
+    df.to_parquet(work_dir + 'ruslawod' + str(counter)  + '.parquet',
+                  engine='pyarrow')
